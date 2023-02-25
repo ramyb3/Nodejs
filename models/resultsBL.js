d2cdbe3b842a758342b72d0b00c40fcb9d671311
ramy <ramy_3@walla.com>
2023-02-25 18:24:01 +0200

    search and result

--- models/resultsBL.js
+++ models/resultsBL.js
@@ -1,34 +1,31 @@
 const restDAL = require("../DAL/restDAL");
 const jsonDAL = require("../DAL/jsonDAL");
 
-const result = async function (obj) {
-  let movie = await jsonDAL.getMovies();
-  let rest = await restDAL.getMovies();
-
+exports.result = async function (obj) {
+  const movie = await jsonDAL.getMovies();
+  const rest = await restDAL.getMovies();
   let data = [],
     genre = [];
 
-  for (i = 0; i < rest.length && i < obj.length; i++) {
+  for (let i = 0; i < rest.length && i < obj.length; i++) {
     data.push(rest.filter((x) => x.id == obj[i]));
   }
 
+  // check if file NewMovies not empty
   if (movie.length != 0) {
-    // check if file NewMovies not empty
-    for (i = 0; i < obj.length; i++) {
+    for (let i = 0; i < obj.length; i++) {
       data.push(movie.movies.filter((x) => x.id == obj[i]));
     }
   }
 
   data = data.flat(); // puting all sub-array in the main array
-  let temp = data.map((x) => x.genres);
+  let arr = data.map((x) => x.genres);
 
-  for (i = 0; i < temp.length; i++) {
-    genre.push(temp.slice(i, i + 1));
+  for (let i = 0; i < arr.length; i++) {
+    genre.push(arr.slice(i, i + 1));
   }
 
   genre = genre.flat(1); // puting all sub-array in the main array
 
   return [data, genre];
 };
-
-module.exports = { result };
