d3135fa6583e6ea26be3e4cbef4ec57f9353c122
ramy <ramy_3@walla.com>
2023-02-25 17:18:56 +0200

    genre file work

--- models/genresBL.js
+++ models/genresBL.js
@@ -5,9 +5,10 @@ exports.genre = async function (obj, data) {
   const movie = await jsonDAL.getMovies();
   const rest = await restDAL.getMovies();
   const restGenre = rest.map((x) => x.genres);
-  let id = [],
-    names = [],
-    temp;
+  const dataId = data.map((x) => x.id);
+  const names = [];
+  const id = [];
+  let temp;
 
   let check; //boolean paramter that returns true/false if the genre exist in the movie data
   let movieGenre = 0; // min length of NewMovies file
@@ -17,68 +18,58 @@ exports.genre = async function (obj, data) {
     movieGenre = movie.movies.map((x) => x.genres);
   }
 
-  for (k = 0; k < obj.length; k++) {
+  for (let k = 0; k < obj.length; k++) {
     temp = [];
 
+    // if there is more than one genre
     if (Array.isArray(obj[k])) {
-      // if there is more than one genre
-      for (j = 0; j < restGenre.length; j++) {
+      for (let j = 0; j < restGenre.length; j++) {
         check = false;
 
-        for (i = 0; i < obj[k].length; i++) {
+        for (let i = 0; i < obj[k].length; i++) {
           check = restGenre[j].includes(obj[k][i]);
 
-          if (check == false) break;
+          if (!check) {
+            break;
+          }
 
-          if (i == obj[k].length - 1 && check == true) {
-            //because there isn't 17th movie, i improvised a little
-
-            if (j < 16) {
-              temp.push(j + 1);
-            }
-
-            if (j >= 16) {
-              temp.push(j + 2);
-            }
+          //because there isn't 17th movie, I improvised a bit
+          if (i == obj[k].length - 1 && check) {
+            temp.push(j + (j < 16 ? 1 : 2));
           }
         }
       }
 
+      // check if file NewMovies not empty
       if (movie.length != 0) {
-        // check if file NewMovies not empty
-        for (j = 0; j < movieGenre.length; j++) {
+        for (let j = 0; j < movieGenre.length; j++) {
           check = false;
 
           for (i = 0; i < obj[k].length; i++) {
             check = movieGenre[j].includes(obj[k][i]);
 
-            if (check == false) break;
+            if (!check) {
+              break;
+            }
 
-            if (i == obj[k].length - 1 && check == true) {
+            if (i == obj[k].length - 1 && check) {
               temp.push(j + 21); // file NewMovies start with id-21
             }
           }
         }
       }
-    } //if there is only one genre
-    else {
-      for (i = 0; i < restGenre.length; i++) {
+    } else {
+      //if there is only one genre
+      for (let i = 0; i < restGenre.length; i++) {
+        //because there isn't 17th movie, I improvised a bit
         if (restGenre[i].includes(obj[k])) {
-          //because there isn't 17th movie, i improvised a little
-
-          if (i < 16) {
-            temp.push(i + 1);
-          }
-
-          if (i >= 16) {
-            temp.push(i + 2);
-          }
+          temp.push(i + (i < 16 ? 1 : 2));
         }
       }
 
+      // check if file NewMovies not empty
       if (movie.length != 0) {
-        // check if file NewMovies not empty
-        for (i = 0; i < movieGenre.length; i++) {
+        for (let i = 0; i < movieGenre.length; i++) {
           if (movieGenre[i].includes(obj[k])) {
             temp.push(i + 21); // file NewMovies start with id-21
           }
@@ -89,32 +80,25 @@ exports.genre = async function (obj, data) {
     id.push(temp);
   }
 
-  let dataId = data.map((x) => x.id);
-
-  for (i = 0; i < id.length; i++) {
-    for (j = 0; j < data.length; j++) {
+  for (let i = 0; i < id.length; i++) {
+    for (let j = 0; j < data.length; j++) {
       temp = [];
 
       if (id[i].length > 1 && id[i].includes(dataId[j]) && i == j) {
-        for (k = 0; k < id[i].length; k++) {
+        for (let k = 0; k < id[i].length; k++) {
+          // get movies from REST API or from NewMovies file
           if (id[i][k] != dataId[j]) {
-            if (id[i][k] < 21) {
-              // get movies from REST API
-              temp.push(rest.find((x) => x.id == id[i][k]));
-            }
-
-            if (id[i][k] > 20) {
-              // get movies from NewMovies file
-              temp.push(movie.movies.find((x) => x.id == id[i][k]));
-            }
+            temp.push(
+              (id[i][k] < 21 ? rest : movie.movies).find(
+                (x) => x.id == id[i][k]
+              )
+            );
           }
         }
       }
-
-      if (temp.length != 0) {
+      if (temp.length > 0) {
         names.push({ id: dataId[j], data: temp });
       }
-
       if (id[i].length == 1 && id[i] == dataId[j]) {
         names.push({
           id: id[i][0],
